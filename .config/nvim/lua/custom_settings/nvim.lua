84dbfc7aa5f86fe3387f55ed2322ba1edc648ae0
David312 <davidhs312yt@gmail.com>
2024-07-16 20:35:08 +0200

    Remove old .config folder

--- .config/nvim/lua/custom_settings/nvim.lua
+++
@@ -1,8 +0,0 @@
--- General config
-local opt = vim.opt
-
-opt.number = true
-opt.shiftwidth = 2
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.expandtab = true
--- .config/nvim/lua/plugin_settings/settings.lua
+++
@@ -1,5 +0,0 @@
--- Load plugins config
-require('plugin_settings.coc')
-require('plugin_settings.nvim-tree')
-require('plugin_settings.onedark')
-require('plugin_settings.lualine')
